8ac383a9915cc4f727cf028b0763814b5624c620
jihansaadani <91476778+jihansaadani@users.noreply.github.com>
2023-11-22 11:07:14 +0100

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+package com.example.java;
+public class Main{
+
+        public static void main(String[] args){
+               System.out.println("Hello "+args[0]+"!");
+               System.out.println("Marhaba "+args[1]+"!");
+        }
+}
